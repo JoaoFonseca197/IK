369f74429dea6029172504a663d706120093a097
João Fonseca <joao.hc.fonseca197@gmail.com>
2024-01-06 02:13:15 +0000

    New scene with contraints

--- Assets/Scripts/CCDIKWithContraints.cs
+++ Assets/Scripts/CCDIKWithContraints.cs
@@ -1,14 +1,15 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CCDIK : MonoBehaviour
+public class CCDIKWithContraints : MonoBehaviour
 {
     [SerializeField] private List<Transform> _listJoints;
     //Left arm constraint hammer down !!
     [SerializeField] private Constraint _armConstraint;
     [SerializeField] private Transform _target;
     [SerializeField] private Transform _endEffectorTip;
-    [Range(0,1)]
+    [Range(0, 1)]
     [SerializeField] private float _errorDistance;
 
     private Vector3 _endEffectorPosition;
@@ -18,13 +19,13 @@ public class CCDIK : MonoBehaviour
         _endEffectorPosition = _endEffectorTip.position;
     }
 
-   
+
     /// <summary>
     /// Forced Clamp of the rotation
     /// </summary>
     /// <param name="rotation">Rotation from the joint</param>
     /// <returns></returns>
-    private Vector3 CheckMaxRotation( Vector3 rotation)
+    private Vector3 CheckMaxRotation(Vector3 rotation)
     {
         float x = Mathf.Clamp(rotation.x, _armConstraint.minX, _armConstraint.maxX);
         float y = Mathf.Clamp(rotation.y, _armConstraint.minY, _armConstraint.maxY);
@@ -37,7 +38,7 @@ public class CCDIK : MonoBehaviour
     {
         Vector3 u1;
         Vector3 u2;
-        if(Vector3.Distance(_target.position,_endEffectorTip.position) >= _errorDistance)
+        if (Vector3.Distance(_target.position, _endEffectorTip.position) >= _errorDistance)
         {
             for (int jointNBR = _listJoints.Count - 1; jointNBR >= 0; jointNBR--)
             {
@@ -45,12 +46,14 @@ public class CCDIK : MonoBehaviour
                 u2 = (_target.position - _listJoints[jointNBR].position);
 
                 _listJoints[jointNBR].rotation = Quaternion.FromToRotation(u1, u2) * _listJoints[jointNBR].rotation;
+                //Just the left arm for test
+                if (jointNBR == 0)
+                    _listJoints[jointNBR].localEulerAngles = CheckMaxRotation(_listJoints[jointNBR].localEulerAngles);
 
-                
+
                 _endEffectorPosition = _endEffectorTip.position;
             }
         }
 
     }
-
 }
